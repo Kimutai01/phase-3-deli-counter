63f169f1a089a7d658e962b7d91a89fe52005b1c
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-11-01 13:54:16 +0300

    Completed assignment

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+def line(array)
+    case array.size
+      when 0 then puts "The line is currently empty."
+       else
+        puts "The line is currently: #{array.map.with_index{|name, index| "#{index+1}. #{name}"}.join(" ")}"
+    end
+end
+
+def take_a_number(array, name)
+    array << name
+    puts "Welcome, #{name}. You are number #{array.size} in line."
+end
+
+def now_serving(array)
+    if array.size == 0
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{array.shift}."
+    end
+end
